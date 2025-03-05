698fc73044d0bf00dc0fa1f72533a8d52c084a0d
AFFIN\lbermejo <lbermejo@affin.es>
2025-03-05 11:17:21 +0100

    se añaden rutas y se crea componente Navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,3 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+import { Navbar } from "./Navbar";
 
 
 export const MainApp = () => {
@@ -5,7 +10,18 @@ export const MainApp = () => {
     return (
        <>
             <h1>MainApp</h1>
+            <Navbar />
             <hr />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
+
+                {/* <Route path="/*" element={<LoginPage />} /> */}
+                <Route path="/*" element={<Navigate to="/about" />} />
+
+            </Routes>
        </> 
     )
 }
